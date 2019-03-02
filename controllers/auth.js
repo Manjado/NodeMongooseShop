0033764a9f3d8e20e0e7d1e20550855db7127fad
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-03-02 10:53:21 +0100

    updated Project Status

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,15 +9,28 @@ console.log(req.session.isLoggedIn)
     });
 };
 
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    });
+};
+
 exports.postLogin = (req, res, next) => {
     User.findById("5c67deb4d4f711368059e06d")
         .then(user => {
             req.session.isLoggedIn = true;
             req.session.user = user;
-            res.redirect('/')
+            req.session.save(err => {
+                console.log(err);
+                res.redirect('/')
+            });
         })
         .catch(err=> console.log(err));
 };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
    req.session.destroy(err => {
