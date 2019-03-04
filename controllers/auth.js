5fd0703734a9d8b662c58a5e831f3b67361e571d
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-03-04 20:31:04 +0100

    adding nodemailer

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,14 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth: {
+        api_key: 'xxxx'
+    }
+}));
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
@@ -82,6 +91,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
             res.redirect('/login');
+            return transporter.sendMail({
+                to: email,
+                from: 'shop@node.com',
+                subject: 'Signup succeeded!',
+                html: '<h1>You successfully signed up!</h1>'
+            })
+            .catch(err => {
+                console.log(err);
+            })
         })
         .catch(err => {
             console.log(err)
