935ffc6011c5da024c6316143fcc754474b0c0af
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-03-08 19:15:13 +0100

    adding logic to update password

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,7 +7,7 @@ const User = require('../models/user');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
-        api_key: 'xxxx'
+        api_key: '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'
     }
 }));
 
@@ -165,3 +165,52 @@ exports.postReset = (req, res, next) => {
             });
     });
 };
+
+exports.getNewPassword = (req, res, next) => {
+    const token = req.params.token;
+    User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+        .then(user => {
+            let message = req.flash('error');
+            if(message.length > 0) {
+                message = message[0]
+            } else {
+                message = null;
+            }
+            res.render('auth/new-password', {
+                path: 'new-password',
+                pageTitle: 'New Password',
+                errorMessage: message,
+                userId: user._id.toString(),
+                passwordToken: token
+            });
+        })
+        .catch(err => console.log(err));
+
+};
+
+exports.postNewPassword = (req, res, next ) => {
+    const newPassword = req.body.password;
+    const userId = req.body.userId;
+    const passwordToken = req.body.passwordToken;
+    let resetUser;
+
+    User.findOne({
+        resetToken: passwordToken,
+        resetTokenExpiration: { $gt: Date.now() },
+        _id: userId
+    })
+    .then(user => {
+        resetUser = user
+        return bcrypt.hash(newPassword, 12);
+    })
+    .then(hashedPassword => {
+        resetUser.password = hashedPassword;
+        resetUser.resetToken = undefined;
+        resetUser.resetTokenExpiration = undefined;
+        return resetUser.save();
+    })
+        .then(result => {
+            res.redirect('/login');
+        })
+    .catch(err => console.log(err))
+}
