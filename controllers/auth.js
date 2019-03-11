b1cd305d74ae9a874a98e650ae88b827284a0e36
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-03-11 06:56:28 +0100

    fix

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ const User = require('../models/user');
 
 const transporter = nodemailer.createTransport(sendgridTransport({
     auth: {
-        api_key: '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'
+        api_key: 'xxxx'
     }
 }));
 
