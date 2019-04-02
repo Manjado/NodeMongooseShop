e259682e55fda9592a946508cb47e4b54498bdaa
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-04-02 21:53:36 +0200

    using the express.js error handling middleware

--- controllers/admin.js
+++ controllers/admin.js
@@ -68,7 +68,10 @@ exports.postAddProduct = (req, res, next) => {
             //     errorMessage: 'Database operation failed, please try again.',
             //     validationErrors: []
             // });
-            res.redirect('/500')
+            //res.redirect('/500')
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
         });
 };
 
@@ -93,7 +96,11 @@ exports.getEditProduct = (req, res, next) => {
                 validationErrors: []
             });
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
 };
 
 exports.postEditProduct = (req, res, next) => {
